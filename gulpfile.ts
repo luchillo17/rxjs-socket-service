34790e173f7608e8f79d32488a4aae862c229a9b
Luchillo <luchillo17@gmail.com>
2018-09-25 22:50:36 -0500

    feat: Update to gulp 4, add TODO file

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,8 +1,20 @@
-import { task } from 'gulp';
+import { task, series, parallel, watch } from 'gulp';
 import * as nodemon from 'gulp-nodemon';
 
-task('run-server', () => {
-  nodemon({
+export function runServer(done) {
+  const server = nodemon({
     script: 'src/server/index.ts',
   });
-});
+  return done();
+}
+
+export function runTest(done) {
+  console.log('====================================');
+  console.log('Exec serie');
+  console.log('====================================');
+  return series(runServer)(done);
+  // TODO add jest for testing before creating the run test task, will depend of jest or angular testBed
+  // return watch
+}
+
+export default series(runServer, runTest);
